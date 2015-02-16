5a3bc4d3257fe6c4a489db2e2a461a36dc68de9c
Rishabh Jain <r@xvf.me>
2015-02-16 18:12:06 +0530

    Flask blueprints

---
+++ app.py
@@ -0,0 +1,15 @@
+from blueprint import main
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main)
+    Bootstrap(app)
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
