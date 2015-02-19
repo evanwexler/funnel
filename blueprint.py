fe794fcf667153ddc37e0c830e77bab56c41c3a5
Rishabh Jain <r@xvf.me>
2015-02-19 18:04:35 +0530

    Infinite scroll

--- blueprint.py
+++ blueprint.py
@@ -2,6 +2,16 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__, template_folder='templates')
 
+crap = ['sdhfiosdufhsifuhsfusfhsidjfhsdlfijsndlfjsdfnlskdjfbnsdkjfhsdkljfhsdljkfhsdbkfjsdbfkjsdbfn.ksdjfbnsd.kjfbsd.kjfbsdkjfbsdkf.jbsdfksbdfk.jsdfb.sdkjfbsd.kjfbsdk.fbsdk.fbsd.fbksdbfsdkfb.sd']
+
 @main.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', crap=crap*30)
+
+@main.route('/3')
+def page3():
+    return render_template('/page3.html')
+
+@main.route('/quiz')
+def quiz():
+    return render_template('/quiz.html')
