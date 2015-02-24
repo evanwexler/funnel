cebba4f361454178ed4b95273a9683255fbf1953
Rishabh Jain <r@xvf.me>
2015-02-24 11:17:10 +0530

    Goodbye waypoint

--- static/main.js
+++ static/main.js
@@ -1,21 +1,9 @@
-
-showMore = function() {
-  url = $('a.nextpage:last').attr('href')
-  x = $.get(url, function(data) {
-    content = $('div.main', data)[0]
-    $("div.main").append(content.innerHTML)
-      })
-    // waypoint.disable()
-    // waypoint.enable()
-}
-
-
-var waypoint = new Waypoint({
-    context: $('div.main'),
-    element: $('div.main .end:last'),
-    offset: '100%',
-    handler: function(d) {
-    showMore()
+$('div.main').bind('scroll', function() {
+    if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+    url = $('a.nextpage:last').attr('href')
+    x = $.get(url, function(data) {
+      content = $('div.main', data)[0]
+      $("div.main").append(content.innerHTML)
+        })
     }
-    })
-
+    });
