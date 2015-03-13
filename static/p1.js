b70408fdaf4086ee401893180a90ab1f8acfe581
Rishabh Jain <r@xvf.me>
2015-03-13 12:39:31 +0530

    Removed apge 1

--- static/p1.js
+++ static/p1.js
@@ -1,34 +1,34 @@
-var cbwaypoint = new Waypoint({
-    context: $("div.main"),
-    element: $("div.colourbar"),
-    handler: function(d) {
-      if (d == 'down') {
-      $("div.main").unbind('scroll', scrollFunc);
-      $('nav.nav').css({'z-index':1});
-        if ($('nav.nav').has('div.colourbar').length == 0) {
-            $('nav.nav').append($('div.colourbar')[0].outerHTML)
-            }
-      }
-      else {
-      $("div.main").bind('scroll', scrollFunc)
-      $('nav.nav').css({'z-index':-1})
-          $('nav.nav div.colourbar').remove()
-      }
-    },
-    offset: '100px',
-    })
+// var cbwaypoint = new Waypoint({
+//     context: $("div.main"),
+//     element: $("div.colourbar"),
+//     handler: function(d) {
+//       if (d == 'down') {
+//       $("div.main").unbind('scroll', scrollFunc);
+//       $('nav.nav').css({'z-index':1});
+//         if ($('nav.nav').has('div.colourbar').length == 0) {
+//             $('nav.nav').append($('div.colourbar')[0].outerHTML)
+//             }
+//       }
+//       else {
+//       $("div.main").bind('scroll', scrollFunc)
+//       $('nav.nav').css({'z-index':-1})
+//           $('nav.nav div.colourbar').remove()
+//       }
+//     },
+//     offset: '100px',
+//     })
 
 var page1height = $("div.page1").innerHeight();
 
 var pagesLoaded = []
 
-scrollFunc = function() {
-    scrolltop = $(this).scrollTop()
-    $("img.mainlogo").height(300-scrolltop)
-    $("div.downarrow").height(200-scrolltop)
-    $("div.page1").height(page1height-scrolltop)
-    $("div.page1").css({'margin-top':scrolltop})
-}
+// scrollFunc = function() {
+//     scrolltop = $(this).scrollTop()
+//     $("img.mainlogo").height(300-scrolltop)
+//     $("div.downarrow").height(200-scrolltop)
+//     $("div.page1").height(page1height-scrolltop)
+//     $("div.page1").css({'margin-top':scrolltop})
+// }
 
 loadNextPage = function() {
     if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
@@ -44,5 +44,5 @@ loadNextPage = function() {
     }
 }
 
-$('div.main').bind('scroll', scrollFunc)
+// $('div.main').bind('scroll', scrollFunc)
 $('div.main').bind('scroll', loadNextPage)
